c7c0ce8095c48a0263441bcc14eb68c033123c22
Axel200121 <axelmartinez212001@gmail.com>
2024-12-26 19:22:13 -0600

    Usando Query Methods

--- src/main/java/com/platzi/market/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/market/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.platzi.market.persistence.entity.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     @Autowired
@@ -12,5 +13,13 @@ public class ProductoRepository {
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidadStock, boolean estado){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidadStock,estado);
     }
 }
--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -3,6 +3,18 @@ package com.platzi.market.persistence.crud;
 import com.platzi.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {
+
+    //ESTO ES UN QUERY METHOD, YA QUE NOS OFRECEN UNA MEJOR FLEXIBILIDAD
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+
+    //PODEMOS HACERLO CON UN anotacion @QUERY
+    // --> @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    // --> List<Producto> findByIdCategory(int idCategory);
 
 }
